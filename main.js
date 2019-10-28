75b39e89554e41b18abdcc82e3e991da6bec78a3
Stanley Ulili <stanleyulili@gmail.com>
2019-10-28 19:51:50 +0200

    (feat): Add questions with answers in array

---
+++ main.js
@@ -0,0 +1,22 @@
+const Questions = [
+    {
+        question: 'Who directed Avengers Endgame?',
+        choices: ['Mark Ruffalo', 'The Russo Brothers', 'Samuel L Jackson', 'Christopher Nolan'],
+        answer: 1,
+    },
+    {
+        question: 'Who directed the Titanic Movie?',
+        choices: ['David Cameron', 'Jamie Foxx', 'The Russo Brothers', 'Taika Watiti'],
+        answer: 0,
+    },
+    {
+        question: 'Who directed  The Dark Knight?',
+        choices: ['Steven Spielberg', 'Brad Cooper', 'Mel Gibson', 'Christopher Nolan'],
+        answer: 4,
+    },
+    {
+        question: 'Who plays Black Widow in Avengers?',
+        choices: ['Emily Brunt', 'Sandra Bullock', 'Scarlet Johannson', 'Angelina Jolie'],
+        answer: 3,
+    }
+]
