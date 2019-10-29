d085b84e3fa16071e2f57d51e54a23a43887ff53
Stanley Ulili <stanleyulili@gmail.com>
2019-10-29 22:09:20 +0200

    (feat): get total number of correct questions

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const quizWrapper = document.querySelector('.quiz');
 
 let questionCount = 1;
 let index = 0;
+let totalScore = 0;
 
 const questions = [
   {
@@ -47,19 +48,19 @@ const questions = [
   },
 ];
 
-function renderQuestion(quiz, index) {
+function renderQuestion(quiz, questionNumber) {
   const markup = `
-<div class="quiz__data">
+<div class="quiz__data" id=${questionNumber}>
  <p class="quiz__tracker">Question ${questionCount} of ${questions.length}</p>
-<div class="quiz__container" id=${index}>
+<div class="quiz__container">
 <h1  class="quiz__question">
 ${quiz.question}
 </h1>
 <form class="quiz__choices">
 ${quiz.choices
     .map(
-      (choice) => `<p>
-    <input type="radio" id="choice" name="answers">
+      (choice, i) => `<p>
+    <input type="radio" id=${i} name="answers" value="${choice}">
     <label for="choice">${choice}</label>
     </p> `,
     )
@@ -76,13 +77,31 @@ renderQuestion(questions[0], 0);
 
 nextBtn.addEventListener('click', () => {
   const quizData = document.querySelector('.quiz__data');
+  const chosenAnswer = document.querySelector('input[type="radio"]:checked').id;
+  const currentQuestion = Number(quizData.id);
+  const chosenAnswerNum = Number(chosenAnswer);
+
   // increment with 1 to get the next question from array
   index += 1;
+
   if ((index < questions.length && questionCount !== questions.length)) {
     questionCount += 1;
-  // remove current question
-  quizData.remove();
-  renderQuestion(questions[index], index);
-  } 
-
+    // remove current question
+    quizData.remove();
+    renderQuestion(questions[index], index);
+    // check if selected value equals to the question answer
+    if (chosenAnswerNum === questions[currentQuestion].answer) {
+      totalScore += 1;
+    }
+  }
 });
+
+
+// option 2
+// get the select answer
+// grab the question id from html
+// compare if selectedAnswer == questions[questionid].answer
+// if true
+// increment totalScore
+// else
+ 
