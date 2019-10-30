6d496cc93aa88b11c7a046d1c88070ccd3a19e80
Stanley Ulili <stanleyulili@gmail.com>
2019-10-30 15:20:26 +0200

    (feat): show progress when clicking next button

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const nextBtn = document.querySelector(".btn-next");
-const quizWrapper = document.querySelector(".quiz");
+const nextBtn = document.querySelector('.btn-next');
+const quizWrapper = document.querySelector('.quiz');
+const progressBar = document.querySelector('progress');
 
 let questionCount = 1;
 let totalScore = 0;
@@ -8,46 +9,48 @@ const userAnswers = [];
 
 const questions = [
   {
-    question: "Who directed Avengers Endgame?",
+    question: 'Who directed Avengers Endgame?',
     choices: [
-      "Mark Ruffalo",
-      "The Russo Brothers",
-      "Samuel L Jackson",
-      "Christopher Nolan"
+      'Mark Ruffalo',
+      'The Russo Brothers',
+      'Samuel L Jackson',
+      'Christopher Nolan'
     ],
     answer: 1
   },
   {
-    question: "Who directed the Titanic Movie?",
+    question: 'Who directed the Titanic Movie?',
     choices: [
-      "David Cameron",
-      "Jamie Foxx",
-      "The Russo Brothers",
-      "Taika Watiti"
+      'David Cameron',
+      'Jamie Foxx',
+      'The Russo Brothers',
+      'Taika Watiti'
     ],
     answer: 0
   },
   {
-    question: "Who directed  The Dark Knight?",
+    question: 'Who directed  The Dark Knight?',
     choices: [
-      "Steven Spielberg",
-      "Brad Cooper",
-      "Mel Gibson",
-      "Christopher Nolan"
+      'Steven Spielberg',
+      'Brad Cooper',
+      'Mel Gibson',
+      'Christopher Nolan'
     ],
     answer: 4
   },
   {
-    question: "Who plays Black Widow in Avengers?",
+    question: 'Who plays Black Widow in Avengers?',
     choices: [
-      "Emily Brunt",
-      "Sandra Bullock",
-      "Scarlet Johannson",
-      "Angelina Jolie"
+      'Emily Brunt',
+      'Sandra Bullock',
+      'Scarlet Johannson',
+      'Angelina Jolie'
     ],
     answer: 3
   }
 ];
+
+progressBar.max = questions.length;
 
 function renderQuestion(quiz, questionNumber) {
   const markup = `
@@ -65,25 +68,38 @@ ${quiz.choices
     <label for="choice">${choice}</label>
     </p> `
   )
-  .join("")}
+  .join('')}
 </form>
 </div>
 </div>
     `;
-  quizWrapper.insertAdjacentHTML("afterbegin", markup);
+  quizWrapper.insertAdjacentHTML('afterbegin', markup);
 }
 
 // show the first question
 renderQuestion(questions[0], 0);
 
 function showWarning() {
-  const choicesForm = document.querySelector(".quiz__choices");
-  const message = `<p class="warning">Please choose an answer!</p>`;
-  choicesForm.insertAdjacentHTML("beforeend", message);
+  const choicesForm = document.querySelector('.quiz__choices');
+  const message = '<p class="warning">Please choose an answer!</p>';
+  choicesForm.insertAdjacentHTML('beforeend', message);
 }
 
-nextBtn.addEventListener("click", () => {
-  const quizData = document.querySelector(".quiz__data");
+/*
+function showProgress(question) {
+ const markup = `
+ <div class="quiz__progressbar">
+ <progress value="${question}" max="${questions.length}"></progress>
+ </div>`;
+ quizButtons.insertAdjacentHTML('afterend', markup);
+} */
+
+function showProgress(question) {
+  progressBar.value = question;
+}
+
+nextBtn.addEventListener('click', () => {
+  const quizData = document.querySelector('.quiz__data');
   const chosenAnswer = document.querySelector('input[type="radio"]:checked');
   let currentQuestion = Number(quizData.id);
   let chosenAnswerNum = null;
@@ -101,12 +117,13 @@ nextBtn.addEventListener("click", () => {
   } else userAnswers.push({ [`question ${currentQuestion + 1}`]: false });
 
   if (currentQuestion === questions.length - 1) {
-    console.log("quiz is finished");
+    console.log('quiz is finished');
   } else {
     // remove current question
     quizData.remove();
     questionCount += 1;
     currentQuestion += 1;
     renderQuestion(questions[currentQuestion], currentQuestion);
+    showProgress(currentQuestion);
   }
 });
