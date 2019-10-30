742c3bc589f011be300dd457aaf136dc5279c696
Stanley Ulili <stanleyulili@gmail.com>
2019-10-30 14:59:43 +0200

    (feat): show error message if user did choose any answer

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const nextBtn = document.querySelector('.btn-next');
-const quizWrapper = document.querySelector('.quiz');
+const nextBtn = document.querySelector(".btn-next");
+const quizWrapper = document.querySelector(".quiz");
 
 let questionCount = 1;
 let totalScore = 0;
@@ -8,45 +8,45 @@ const userAnswers = [];
 
 const questions = [
   {
-    question: 'Who directed Avengers Endgame?',
+    question: "Who directed Avengers Endgame?",
     choices: [
-      'Mark Ruffalo',
-      'The Russo Brothers',
-      'Samuel L Jackson',
-      'Christopher Nolan',
+      "Mark Ruffalo",
+      "The Russo Brothers",
+      "Samuel L Jackson",
+      "Christopher Nolan"
     ],
-    answer: 1,
+    answer: 1
   },
   {
-    question: 'Who directed the Titanic Movie?',
+    question: "Who directed the Titanic Movie?",
     choices: [
-      'David Cameron',
-      'Jamie Foxx',
-      'The Russo Brothers',
-      'Taika Watiti',
+      "David Cameron",
+      "Jamie Foxx",
+      "The Russo Brothers",
+      "Taika Watiti"
     ],
-    answer: 0,
+    answer: 0
   },
   {
-    question: 'Who directed  The Dark Knight?',
+    question: "Who directed  The Dark Knight?",
     choices: [
-      'Steven Spielberg',
-      'Brad Cooper',
-      'Mel Gibson',
-      'Christopher Nolan',
+      "Steven Spielberg",
+      "Brad Cooper",
+      "Mel Gibson",
+      "Christopher Nolan"
     ],
-    answer: 4,
+    answer: 4
   },
   {
-    question: 'Who plays Black Widow in Avengers?',
+    question: "Who plays Black Widow in Avengers?",
     choices: [
-      'Emily Brunt',
-      'Sandra Bullock',
-      'Scarlet Johannson',
-      'Angelina Jolie',
+      "Emily Brunt",
+      "Sandra Bullock",
+      "Scarlet Johannson",
+      "Angelina Jolie"
     ],
-    answer: 3,
-  },
+    answer: 3
+  }
 ];
 
 function renderQuestion(quiz, questionNumber) {
@@ -59,46 +59,54 @@ ${quiz.question}
 </h1>
 <form class="quiz__choices">
 ${quiz.choices
-    .map(
-      (choice, i) => `<p>
+  .map(
+    (choice, i) => `<p>
     <input type="radio" id=${i} name="answers" value="${choice}">
     <label for="choice">${choice}</label>
-    </p> `,
-    )
-    .join('')}
+    </p> `
+  )
+  .join("")}
 </form>
 </div>
 </div>
     `;
-  quizWrapper.insertAdjacentHTML('afterbegin', markup);
+  quizWrapper.insertAdjacentHTML("afterbegin", markup);
 }
 
 // show the first question
 renderQuestion(questions[0], 0);
 
-nextBtn.addEventListener('click', () => {
-  const quizData = document.querySelector('.quiz__data');
+function showWarning() {
+  const choicesForm = document.querySelector(".quiz__choices");
+  const message = `<p class="warning">Please choose an answer!</p>`;
+  choicesForm.insertAdjacentHTML("beforeend", message);
+}
+
+nextBtn.addEventListener("click", () => {
+  const quizData = document.querySelector(".quiz__data");
   const chosenAnswer = document.querySelector('input[type="radio"]:checked');
   let currentQuestion = Number(quizData.id);
-  const chosenAnswerNum = Number(chosenAnswer.id);
+  let chosenAnswerNum = null;
+  
+  if (chosenAnswer) {
+    chosenAnswerNum = Number(chosenAnswer.id);
+  } else {
+    showWarning();
+    return;
+  }
 
   if (chosenAnswerNum === questions[currentQuestion].answer) {
     totalScore += 1;
     userAnswers.push({ [`question ${currentQuestion + 1}`]: true });
   } else userAnswers.push({ [`question ${currentQuestion + 1}`]: false });
-  // remove current question
-  quizData.remove();
-  questionCount += 1;
-  currentQuestion += 1;
-  renderQuestion(questions[currentQuestion], currentQuestion);
+
+  if (currentQuestion === questions.length - 1) {
+    console.log("quiz is finished");
+  } else {
+    // remove current question
+    quizData.remove();
+    questionCount += 1;
+    currentQuestion += 1;
+    renderQuestion(questions[currentQuestion], currentQuestion);
+  }
 });
-
-
-// option 2
-// get the select answer
-// grab the question id from html
-// compare if selectedAnswer == questions[questionid].answer
-// if true
-// increment totalScore
-// else
- 
